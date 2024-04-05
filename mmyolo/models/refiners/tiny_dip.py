49b09c59d9ce859e22e4306cf2f8cb2c223da48b
qninhdt <qndt123@gmail.com>
2024-04-06 01:47:37 +0700

    cc

--- mmyolo/models/refiners/tiny_dip.py
+++ mmyolo/models/refiners/tiny_dip.py
@@ -1,5 +1,5 @@
 import torch
-import torchvision.transforms.functional as F
+import torch.nn.functional as F
 
 from mmyolo.registry import MODELS
 
@@ -9,9 +9,38 @@ class TinyDIP(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
+        self.conv1 = torch.nn.Conv2d(3, 8, 3, padding=1)
+        self.conv2 = torch.nn.Conv2d(8, 16, 3, padding=1)
+        self.conv3 = torch.nn.Conv2d(16, 32, 3, padding=1)
+
+        # alpha, beta, gamma
+        self.linear1 = torch.nn.Linear(32, 3)
+
+    def compute_params(self, x):
+        x = F.interpolate(x, scale_factor=0.125)  # 1/8
+
+        x = F.relu(self.conv1(x))  # 1/16
+        x = F.relu(self.conv2(x))  # 1/32
+        x = F.relu(self.conv3(x))  # 1/64
+
+        x = torch.nn.functional.adaptive_avg_pool2d(x, (1, 1))
+        x = x.flatten(1)
+
+        x = self.linear1(x)
+
+        return x
+
     def forward(self, x):
-        # convert to uint8
-        uint_x = (x * 255).to(torch.uint8)
-        uint_x = torch.stack([F.equalize(uint_x[i]) for i in range(x.size(0))])
-        x = uint_x.float() / 255
+        params = self.compute_params(x)
+
+        alpha = params[:, 0].view(-1, 1, 1, 1)
+        beta = params[:, 1].view(-1, 1, 1, 1)
+        gamma = params[:, 2].view(-1, 1, 1, 1)
+
+        # I(x) = alpha * I(x) + beta
+        x = alpha * x + beta
+
+        # I(x) = I(x) ** gamma
+        x = x**gamma
+
         return x
--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
-python tools/train.py configs/yolov7/yolov7_tiny_syncbn_fast_1x16b-100e_exdark.py \
-    --amp 
+python tools/train.py configs/lla_yolo/lla_yolo_tiny_syncbn_fast_tiny_dip_1x16b-100e_exdark.py \
+    --amp
+    # --cfg-options
         # visualizer.vis_backends.0.init_kwargs.id="4gjriogu"
     # --resume auto \
