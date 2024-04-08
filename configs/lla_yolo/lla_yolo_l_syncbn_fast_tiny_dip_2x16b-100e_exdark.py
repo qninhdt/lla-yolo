2aec90a594b89f19cff71bced1f1c7b76e7a54b6
qninhdt <qndt123@gmail.com>
2024-04-08 18:43:56 +0700

    a

--- configs/lla_yolo/lla_yolo_l_syncbn_fast_tiny_dip_2x16b-100e_exdark.py
+++ configs/lla_yolo/lla_yolo_l_syncbn_fast_tiny_dip_2x16b-100e_exdark.py
@@ -1,4 +1,4 @@
-_base_ = "../yolov7/yolov7_l_syncbn_fast_1x16b-100e_exdark.py"
+_base_ = "../yolov7/yolov7_l_syncbn_fast_2x16b-100e_exdark.py"
 
 _base_.model.type = "LLAYOLODetector"
 _base_.model.refiner = dict(type="TinyDIP")
